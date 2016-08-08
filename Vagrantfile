38fba8677c7e26aa593bdd9fd0af1efeeabc6c4d
Mizhal <mizhal@gmail.com>
2016-08-08 15:08:36 +0200

    update

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,7 @@ ip = "192.168.7.3"
 
 Vagrant.configure(2) do |config|
 
-  config.vm.box = "geerlingguy/ubuntu1604"
+  config.vm.box = "ubuntu/wily64-juju"
   config.vm.box_check_update = false
   config.vm.hostname = "jupyter-local.com"
   config.hostsupdater.aliases = ["data.local-notebook.com"]
@@ -38,5 +38,7 @@ Vagrant.configure(2) do |config|
   config.vm.provision "install-daemon", type: "shell" do |id|
     id.path = "./install-daemon.sh"
   end
+
+  config.vm.provision :reload
   
 end
--- install-python.sh
+++ install-python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y python python-pip python-virtualenv
+apt-get install -y build-essential python python-pip python-virtualenv python-dev python2.7 python2.7-dev
 
 ## Matplotlib dependencies
 apt-get install -y pkg-config ## matplotlib won't find freetype without this
@@ -11,6 +11,4 @@ apt-get install -y python-pyqt5
 apt-get install -y dvipng poppler-utils ghostscript
 
 ## the new matlab
-pip install jupyter
-pip install matplotlib
-pip install numpy
+pip install markupsafe zmq jsonschema jupyter matplotlib numpy ipykernel
